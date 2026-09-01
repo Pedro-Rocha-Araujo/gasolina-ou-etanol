743a519c7c666c8952c2e4ffc8db09e4c263272a
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-09-01 11:54:01 -0300

    Mudanças no css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 export default function Home() {
   return (
     <main>
+
       <div className="imagem">
         <img src={"./imagem.png"} />
       </div>
+
       <form>
-        <input />
-        <input />
-        <button>
-          Enviar
-        </button>
+        <input type="number" placeholder="Valor da Gasolina" />
+        <input type="number" placeholder="Valor do Etanol " />
+        <button>Conferir</button>
       </form>
+
     </main>
   )
 }
