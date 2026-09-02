e7af317954e430385fb73efe7e329afabe51cb1d
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-09-02 08:08:24 -0300

    Card para a mensagem estruturado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,34 @@
+'use client'
+
+import { useState } from "react"
+import Card from "@/components/Card"
+
 export default function Home() {
+  const [card, setCard] = useState<boolean>(true)
+
   return (
     <main>
+      { card === true && (
+        <Card />
+      ) }
 
       <div className="imagem">
         <img src={"./imagem.png"} />
       </div>
 
       <form>
-        <input type="number" placeholder="Valor da Gasolina" />
-        <input type="number" placeholder="Valor do Etanol " />
+        <input 
+          type="number" 
+          placeholder="Valor da Gasolina"
+          required 
+        />
+
+        <input 
+          type="number" 
+          placeholder="Valor do Etanol"
+          required 
+        />
+
         <button>Conferir</button>
       </form>
 
---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import "./card.css"
+
+export default function Card() {
+  return (
+    <div className="background">
+      <div className="card">
+        <h1>Gasolina</h1>
+        <p>Com relação ao valores passados, no momento a gasolina torna-se mais vantajosa.</p>
+        <button>Fechar</button>
+      </div>
+    </div>
+  )
+}
