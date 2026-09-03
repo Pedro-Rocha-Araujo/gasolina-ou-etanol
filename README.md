183ed67af2c6d0fd709dfa7ba7e43d099abb970d
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-09-03 10:27:51 -0300

    Adicionei melhor responsividade nas fontes do projeto utilizando o clamp

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # ⛽ Gasolina ou Alcool?
 ## O presente projeto trata-se de uma calculadora em tempo rel para descobrir a forma mais econômica de abastecer seu carro.
 <img width="1920" height="911" alt="thumbnail" src="https://github.com/user-attachments/assets/2ce44ceb-7792-45f0-8e9a-1c54f9ae445f" />
+
 ### Funcionamento:
 1- Digite o valor do litro da Gasolina
 2- Digite o valor do litro do Etanol
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,7 @@ export default function Card({ resultado, fechar }: CardProps) {
   return (
     <div className="background">
       <div className="card">
-        <h1><i className="fa-solid fa-gas-pump"></i> { resultado }</h1>
+        <h2><i className="fa-solid fa-gas-pump"></i> { resultado }</h2>
         <p>Após relacionar os valores, concluímos que no momento torna-se mais vantajoso usar {resultado}.</p>
         <button onClick={fechar}>Fechar</button>
       </div>
